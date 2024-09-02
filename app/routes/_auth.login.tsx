0d3a9f4b87419c2b19d9c9dfedf7a842d1900691
Marek Zelinka <mzelinka17@gmail.com>
2024-09-02 13:38:34 +0200

    login

--- app/routes/_auth.login.tsx
+++ app/routes/_auth.login.tsx
@@ -12,31 +12,10 @@ import { Logo } from '~/components/logo';
 import { Button } from '~/components/ui/button';
 import { Input } from '~/components/ui/input';
 import { Label } from '~/components/ui/label';
-import { createUser, createUserSession } from '~/utils/auth.server';
-import { prisma } from '~/utils/db.server';
+import { createUserSession, verifyLogin } from '~/utils/auth.server';
 import { composeSafeRedirectUrl } from '~/utils/misc';
 
-const CreateAccountSchema = z.object({
-  username: z
-    .string({ required_error: 'Username is required' })
-    .min(3, 'Username is too short')
-    .max(20, 'Username is too long')
-    .regex(
-      /^[a-zA-Z0-9_]+$/,
-      'Username can only include letters, numbers, and underscores',
-    )
-    // Users can type the username in any case, but we store it in lowercase
-    .transform((value) => value.toLowerCase()),
-  first: z
-    .string({ required_error: 'First name is required' })
-    .trim()
-    .min(3, 'First name is too short')
-    .max(40, 'Last name is too long'),
-  last: z
-    .string({ required_error: 'Last name is required' })
-    .trim()
-    .min(3, 'Last name is too short')
-    .max(40, 'Last name is too long'),
+const LoginSchema = z.object({
   email: z
     .string({ required_error: 'Email is required' })
     .trim()
@@ -50,7 +29,7 @@ const CreateAccountSchema = z.object({
     .min(6, 'Password is too short'),
 });
 
-export const meta: MetaFunction = () => [{ title: 'Sign up' }];
+export const meta: MetaFunction = () => [{ title: 'Login' }];
 
 export async function action({ request }: ActionFunctionArgs) {
   const url = new URL(request.url);
@@ -58,36 +37,19 @@ export async function action({ request }: ActionFunctionArgs) {
 
   const formData = await request.formData();
   const submission = await parseWithZod(formData, {
-    schema: CreateAccountSchema.superRefine(async (arg, ctx) => {
-      const userWithSameEmail = await prisma.user.findUnique({
-        select: { id: true },
-        where: { email: arg.email },
-      });
+    schema: LoginSchema.transform(async (arg, ctx) => {
+      const user = await verifyLogin(arg.email, arg.password);
 
-      if (userWithSameEmail) {
+      if (!user) {
         ctx.addIssue({
-          path: ['email'],
           code: z.ZodIssueCode.custom,
-          message: 'A user already exists with this email',
+          message: 'Invalid username or password',
         });
 
         return z.NEVER;
       }
 
-      const userWithSameUsername = await prisma.user.findUnique({
-        select: { id: true },
-        where: { username: arg.username },
-      });
-
-      if (userWithSameUsername) {
-        ctx.addIssue({
-          path: ['username'],
-          code: z.ZodIssueCode.custom,
-          message: 'A user already exists with this username',
-        });
-
-        return z.NEVER;
-      }
+      return { ...arg, user };
     }),
     async: true,
   });
@@ -99,14 +61,12 @@ export async function action({ request }: ActionFunctionArgs) {
     );
   }
 
-  const { username, first, last, email, password } = submission.value;
-
-  const user = await createUser({ username, first, last, email, password });
+  const { user } = submission.value;
 
   return createUserSession({
     request,
     userId: user.id,
-    remember: false,
+    remember: true,
     redirectTo,
   });
 }
@@ -115,12 +75,12 @@ export default function Component() {
   const actionData = useActionData<typeof action>();
 
   const [form, fields] = useForm({
-    constraint: getZodConstraint(CreateAccountSchema),
+    constraint: getZodConstraint(LoginSchema),
     lastResult: actionData?.result,
     shouldValidate: 'onBlur',
     shouldRevalidate: 'onInput',
     onValidate: ({ formData }) => {
-      return parseWithZod(formData, { schema: CreateAccountSchema });
+      return parseWithZod(formData, { schema: LoginSchema });
     },
   });
 
@@ -131,53 +91,15 @@ export default function Component() {
       <div className="mx-auto w-full max-w-[400px]">
         <Logo className="mx-auto h-9 w-auto" />
         <div className="mt-6 text-center">
-          <h1 className="text-3xl font-bold">Sign Up</h1>
+          <h1 className="text-3xl font-bold">Login</h1>
           <p className="mt-2 text-muted-foreground">
-            Enter your information to create an account
+            Enter your email below to login to your account
           </p>
         </div>
       </div>
       <div className="mx-auto mt-6 w-full max-w-[400px]">
         <Form method="post" {...getFormProps(form)}>
           <div className="grid gap-4">
-            <div className="grid gap-2">
-              <Label htmlFor={fields.username.id}>Username</Label>
-              <Input
-                autoComplete="username"
-                placeholder="m_robinson"
-                {...getInputProps(fields.username, { type: 'text' })}
-              />
-              <ErrorList
-                id={fields.username.errorId}
-                errors={fields.username.errors}
-              />
-            </div>
-            <div className="grid grid-cols-2 items-start gap-4">
-              <div className="grid gap-2">
-                <Label htmlFor={fields.first.id}>First name</Label>
-                <Input
-                  autoComplete="given-name"
-                  placeholder="Max"
-                  {...getInputProps(fields.first, { type: 'text' })}
-                />
-                <ErrorList
-                  id={fields.first.errorId}
-                  errors={fields.first.errors}
-                />
-              </div>
-              <div className="grid gap-2">
-                <Label htmlFor={fields.last.id}>Last name</Label>
-                <Input
-                  autoComplete="family-name"
-                  placeholder="Robinson"
-                  {...getInputProps(fields.last, { type: 'text' })}
-                />
-                <ErrorList
-                  id={fields.last.errorId}
-                  errors={fields.last.errors}
-                />
-              </div>
-            </div>
             <div className="grid gap-2">
               <Label htmlFor={fields.email.id}>Email</Label>
               <Input
@@ -204,19 +126,19 @@ export default function Component() {
             <ErrorList id={form.errorId} errors={form.errors} />
             <div>
               <Button type="submit" className="w-full">
-                Create an account
+                Login
               </Button>
             </div>
           </div>
         </Form>
       </div>
       <p className="mt-4 text-center text-sm">
-        Already have an account?{' '}
+        Don&apos;t have an account?{' '}
         <Link
-          to={{ pathname: '/login', search: searchParams.toString() }}
+          to={{ pathname: '/join', search: searchParams.toString() }}
           className="underline"
         >
-          Sign in
+          Sign up
         </Link>
       </p>
     </>
