137bf78b4ab305d52167dcea5681e0c87125ef75
Marek Zelinka <mzelinka17@gmail.com>
2024-09-10 11:41:09 +0200

    trun off logs from prisma

--- app/lib/db.server.ts
+++ app/lib/db.server.ts
@@ -5,9 +5,7 @@ export const prisma = remember('prisma', () => {
   // NOTE: if you change anything in this function you'll need to restart
   // the dev server to see your changes.
 
-  const client = new PrismaClient({
-    log: ['query', 'info', 'warn', 'error'],
-  });
+  const client = new PrismaClient();
   void client.$connect();
 
   return client;
