06a61d840ac3d032b25a105f91c6887bc3104283
Marek Zelinka <mzelinka17@gmail.com>
2024-09-11 12:48:51 +0200

    remove console log

--- app/routes/_dashboard.contacts.$contactId_.edit.tsx
+++ app/routes/_dashboard.contacts.$contactId_.edit.tsx
@@ -179,8 +179,6 @@ export default function Component() {
 
   const actionData = useActionData<typeof action>();
 
-  console.log(contact);
-
   const [form, fields] = useForm({
     defaultValue: {
       ...contact,
