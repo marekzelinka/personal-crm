c9956a4c3fab11da924cb612c73aa22291d9782f
Marek Zelinka <mzelinka17@gmail.com>
2024-09-12 20:03:54 +0200

    add dropdown with actions to note items

--- app/lib/utils.ts
+++ app/lib/utils.ts
@@ -1,4 +1,5 @@
 import { defineConfig } from 'cva';
+import { useCallback, useEffect, useState } from 'react';
 import { twMerge } from 'tailwind-merge';
 
 export type { VariantProps } from 'cva';
@@ -8,6 +9,33 @@ export const { cva, cx, compose } = defineConfig({
     onComplete: twMerge,
   },
 });
+
+export function useClipboard(value: string, timeout = 1500) {
+  const [{ state, i }, setState] = useState<{
+    state: 'idle' | 'copied';
+    i: number;
+  }>({ state: 'idle', i: 0 });
+  const hasCopied = state === 'copied';
+
+  const copy = useCallback(async () => {
+    await navigator.clipboard.writeText(value);
+    setState({ state: 'copied', i: i + 1 });
+  }, [i, value]);
+
+  useEffect(() => {
+    if (hasCopied) {
+      const timeoutId = window.setTimeout(() => {
+        setState({ state: 'idle', i: i + 1 });
+      }, timeout);
+
+      return function cleanup() {
+        window.clearTimeout(timeoutId);
+      };
+    }
+  }, [hasCopied, i, timeout]);
+
+  return { copy, hasCopied };
+}
 
 /**
  * This should be used any time the redirect path is user-provided
--- app/routes/_dashboard.contacts.$contactId.notes.tsx
+++ app/routes/_dashboard.contacts.$contactId.notes.tsx
@@ -1,18 +1,33 @@
 import { parseWithZod } from '@conform-to/zod';
 import { invariant, invariantResponse } from '@epic-web/invariant';
+import { DotsHorizontalIcon, UpdateIcon } from '@radix-ui/react-icons';
 import {
   json,
   type ActionFunctionArgs,
   type LoaderFunctionArgs,
   type SerializeFrom,
 } from '@remix-run/node';
-import { useActionData, useFetchers, useLoaderData } from '@remix-run/react';
+import {
+  Link,
+  useActionData,
+  useFetchers,
+  useLoaderData,
+} from '@remix-run/react';
 import { compareAsc, format, isToday, isYesterday } from 'date-fns';
+import { useSpinDelay } from 'spin-delay';
 import { EmptyState } from '~/components/empty-state';
 import { NoteForm, NoteFormSchema } from '~/components/note-form';
+import { Button } from '~/components/ui/button';
 import { Card, CardContent, CardHeader, CardTitle } from '~/components/ui/card';
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuTrigger,
+} from '~/components/ui/dropdown-menu';
 import { requireUserId } from '~/lib/auth.server';
 import { db } from '~/lib/db.server';
+import { useClipboard } from '~/lib/utils';
 
 type LoaderData = SerializeFrom<typeof loader>;
 type Note = LoaderData['notes'][number];
@@ -65,8 +80,9 @@ export default function Component() {
 
   return (
     <Card>
-      <CardHeader>
+      <CardHeader className="flex-row items-center justify-between">
         <CardTitle>Notes</CardTitle>
+        <NoteSavingIndicator />
       </CardHeader>
       <CardContent className="grid gap-8">
         <NoteForm lastResult={actionData?.result} />
@@ -76,13 +92,26 @@ export default function Component() {
   );
 }
 
+function NoteSavingIndicator() {
+  const optimisticNotes = useOptimisticNotes();
+  const showShowIndicator = useSpinDelay(optimisticNotes.length > 0);
+
+  if (!showShowIndicator) {
+    return null;
+  }
+
+  return (
+    <UpdateIcon className="animate-spin text-muted-foreground" aria-hidden />
+  );
+}
+
 function NoteList() {
   const { notes } = useLoaderData<typeof loader>();
 
+  const optimisticNotes = useOptimisticNotes();
   const notesById = new Map(notes.map((note) => [note.id, note]));
 
   // Merge optimistic and existing entries
-  const optimisticNotes = useOptimisticNotes();
   for (const optimisticNote of optimisticNotes) {
     const note = notesById.get(optimisticNote.id);
     const merged = note ? { ...note, ...optimisticNote } : optimisticNote;
@@ -94,30 +123,17 @@ function NoteList() {
       compareAsc(b.date, a.date) || compareAsc(b.createdAt, a.createdAt),
   );
 
-  if (!notesToShow.length) {
-    return (
-      <EmptyState
-        title="No notes"
-        description="You haven’t saved any notes yet."
-      />
-    );
-  }
-
-  return (
+  return notesToShow.length ? (
     <ul className="grid gap-8">
       {notesToShow.map((note) => (
-        <li key={note.id} className="grid gap-1">
-          <div className="block text-xs text-muted-foreground">
-            {isToday(note.date)
-              ? 'today'
-              : isYesterday(note.date)
-                ? 'yesterday'
-                : format(note.date, 'PP')}
-          </div>
-          <div className="whitespace-pre-wrap text-sm">{note.text}</div>
-        </li>
+        <NoteItem key={note.id} note={note} />
       ))}
     </ul>
+  ) : (
+    <EmptyState
+      title="No notes"
+      description="You haven’t saved any notes yet."
+    />
   );
 }
 
@@ -146,4 +162,41 @@ function useOptimisticNotes() {
       };
     })
     .filter((note) => note !== null);
+}
+
+function NoteItem({ note }: { note: Note }) {
+  const { copy } = useClipboard(note.text);
+
+  return (
+    <li key={note.id} className="flex gap-4">
+      <div className="flex-auto py-1 text-sm">{note.text}</div>
+      <div className="flex flex-none items-center gap-2">
+        <p className="text-sm text-muted-foreground">
+          {isToday(note.date)
+            ? 'today'
+            : isYesterday(note.date)
+              ? 'yesterday'
+              : format(note.date, 'PP')}
+        </p>
+        <DropdownMenu>
+          <DropdownMenuTrigger asChild>
+            <Button
+              size="icon"
+              variant="ghost"
+              className="size-7"
+              aria-label="Toggle menu"
+            >
+              <DotsHorizontalIcon aria-hidden />
+            </Button>
+          </DropdownMenuTrigger>
+          <DropdownMenuContent align="end">
+            <DropdownMenuItem asChild>
+              <Link to="edit">Edit</Link>
+            </DropdownMenuItem>
+            <DropdownMenuItem onClick={copy}>Copy</DropdownMenuItem>
+          </DropdownMenuContent>
+        </DropdownMenu>
+      </div>
+    </li>
+  );
 }
